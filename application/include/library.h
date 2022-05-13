0abdec957385a608b9f1b30565752e2ca0fb498d
RubenRivas <rudrivas@hotmail.com>
2022-05-13 17:49:59 -0500

    Initial commit

---
+++ application/include/library.h
@@ -0,0 +1,8 @@
+#ifndef INC_2_APPLICATION_LAYER_LIBRARY_H
+#define INC_2_APPLICATION_LAYER_LIBRARY_H
+
+#include <string>
+using namespace std;
+void hello(string texto);
+
+#endif //INC_2_APPLICATION_LAYER_LIBRARY_H
---
+++ application/src/library.cpp
@@ -0,0 +1,9 @@
+#include "../include/library.h"
+
+#include <iostream>
+#include <string>
+
+
+void hello(string param) {
+    std::cout << "Hello," << param << " !" << std::endl;
+}
